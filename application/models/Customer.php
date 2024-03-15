10ee02f0a3afafa4dd16b831bd1f95ea2359ce27
fikripik <fikri.rizky88@gmail.com>
2024-03-15 18:45:39 +0700

    button reset password

--- application/models/Customer.php
+++ application/models/Customer.php
@@ -47,8 +47,8 @@ class Customer extends CI_Model
             $mail->isSMTP();
             $mail->Host       = 'smtp.gmail.com';
             $mail->SMTPAuth   = true;
-            $mail->Username   = 'rianbasari27@gmail.com';
-            $mail->Password   = 'tkhvsvcvsaebqpbn';
+            $mail->Username   = 'hai.seidea@gmail.com';
+            $mail->Password   = 'vkhqmuasylbxfcyc';
             $mail->SMTPSecure = 'tls';
             $mail->Port       = 587;            
             $mail->SMTPOptions = array(
@@ -59,13 +59,63 @@ class Customer extends CI_Model
                     )
             );
 
-            $mail->setFrom('rianbasari27@gmail.com', 'Admin Hi-Trip');
+            $mail->setFrom('hai.seidea@gmail.com', 'Admin Hi-Trip');
             $mail->addAddress($email, $user->name);
             
             $mail->isHTML(true);
             $link = base_url("jamaah/login/reset_password?mail=$email");
-            $mail->Subject = 'Reset My Password';
-            $mail->Body    = "Klik link berikut untuk reset Password, <a href='$link'>Klik untuk reset Password<a>";
+            $mail->Subject = 'Reset Password';
+            $mail->Body = "
+                            <!DOCTYPE html>
+                            <html lang='id'>
+                            <head>
+                                <meta charset='UTF-8'>
+                                <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+                                <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+                                <title>Reset Password</title>
+                                <style>
+                                    body {
+                                        font-family: Arial, sans-serif;
+                                        line-height: 1.6;
+                                        background-color: #f7f7f7;
+                                        margin: 0;
+                                        padding: 0;
+                                        -webkit-font-smoothing: antialiased;
+                                        -moz-osx-font-smoothing: grayscale;
+                                    }
+                                    .container {
+                                        max-width: 600px;
+                                        margin: 20px auto;
+                                        padding: 20px;
+                                        background-color: #fff;
+                                        border-radius: 8px;
+                                        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                                    }
+                                    h2 {
+                                        color: #333;
+                                    }
+                                    p {
+                                        color: #666;
+                                    }
+                                    .button {
+                                        display: inline-block;
+                                        padding: 10px 20px;
+                                        background-color: #007bff;
+                                        color: #fff;
+                                        text-decoration: none;
+                                        border-radius: 5px;
+                                    }
+                                </style>
+                            </head>
+                            <body>
+                                <div class='container'>
+                                    <h2>Reset Password</h2>
+                                    <p>Klik link berikut untuk mereset password Anda:</p>
+                                    <p><a class='button' href='$link'>Klik untuk reset Password</a></p>
+                                </div>
+                            </body>
+                            </html>
+                            ";
 
 
             $mail->send();
