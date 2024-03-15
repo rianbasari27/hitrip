74df22ff93492c90b8b0030cf354946ba9a9bae0
fikripik <fikri.rizky88@gmail.com>
2024-03-15 19:26:32 +0700

    p

--- application/views/jamaah/include/header.php
+++ application/views/jamaah/include/header.php
@@ -5,7 +5,7 @@
 <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 <meta name="viewport"
     content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover" />
-<link rel=”icon” href="<?php echo base_url() . 'asset/icons/android/icon-title.png';?>">
+<link rel="icon" href="<?php echo base_url() . 'asset/icons/android/icon-title.png';?>" type="image/x-icon">
 <title>Hi Trip</title>
 
 
