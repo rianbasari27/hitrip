a449b5c9bcff73d92c8c5f1e1e4d39564afcbce2
fikripik <fikri.rizky88@gmail.com>
2024-04-30 14:23:34 +0700

    p

--- application/views/jamaah/jadwal_sholat.php
+++ application/views/jamaah/jadwal_sholat.php
@@ -4,9 +4,9 @@
 <head>
     <?php $this->load->view('jamaah/include/header'); ?>
     <style>
-        #page {
-            min-height: 100vh !important;
-        }
+    #page {
+        min-height: 100vh !important;
+    }
     </style>
 </head>
 
@@ -35,7 +35,7 @@
                 <p id="tgl-container" class="mb-n2"></p>
             </div>
             <div id="result-container"></div>
-            
+
             <?php $this->load->view('jamaah/include/footer'); ?>
         </div>
 
@@ -51,11 +51,11 @@
         <!-- Colors Menu-->
         <div id="menu-colors" class="menu menu-box-bottom rounded-m"
             data-menu-load="<?php echo base_url() . 'jamaah/menu/colors'; ?>" data-menu-height="480"></div>
-        
+
     </div>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> 
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <?php $this->load->view('jamaah/include/script_view'); ?>
     <script>
     function getCityAndCountry(latitude, longitude) {
@@ -81,18 +81,72 @@
                         displayPrayerTimes(jadwal, timezone, city, country, d);
                     })
                     .catch(error => {
-                        // console.error("Error getting city and country: ", error);
-                        const jakartaLatitude = -6.186486;
-                        const jakartaLongitude = 106.834091;
-                        getCityAndCountry(jakartaLatitude, jakartaLongitude);
+                        var jakartaLatitude = -6.186486;
+                        var jakartaLongitude = 106.834091;
+                        var solatApiErr =
+                            `https://api.aladhan.com/v1/timings/${tanggal}?latitude=${jakartaLatitude}&longitude=${jakartaLongitude}&method=20&tune=1,1,1,1,1,1,1,1`;
+                        var apiUrlErr =
+                            `https://nominatim.openstreetmap.org/reverse?format=json&lat=${jakartaLatitude}&lon=${jakartaLongitude}&accept-language=id`;
+                        fetch(solatApiErr)
+                            .then(response => response.json())
+                            .then(data => {
+                                fetch(apiUrlErr)
+                                    .then(response => response.json())
+                                    .then(location => {
+                                        console.log(data.data);
+                                        var jadwal = data.data.timings;
+                                        var timezone = data.data.meta.timezone;
+                                        var city = location.address.city || location.address
+                                            .city_district;
+                                        var country = location.address.country;
+                                        displayPrayerTimes(jadwal, timezone, city, country, d);
+                                    })
+                                    .catch(error => {
+                                        // console.error("Error getting city and country: ", error);
+                                        const jakartaLatitude = -6.186486;
+                                        const jakartaLongitude = 106.834091;
+                                        getCityAndCountry(jakartaLatitude, jakartaLongitude);
+                                        // Penanganan kesalahan jika gagal mendapatkan informasi kota dan negara
+                                    });
+                            })
+                            .catch(error => {
+                                console.error("Error getting city and country: ", error);
+                            });
                         // Penanganan kesalahan jika gagal mendapatkan informasi kota dan negara
                     });
             })
             .catch(error => {
-                // console.error("Error getting city and country: ", error);
-                const jakartaLatitude = -6.186486;
-                const jakartaLongitude = 106.834091;
-                getCityAndCountry(jakartaLatitude, jakartaLongitude);
+                var jakartaLatitude = -6.186486;
+                var jakartaLongitude = 106.834091;
+                var solatApiErr =
+                    `https://api.aladhan.com/v1/timings/${tanggal}?latitude=${jakartaLatitude}&longitude=${jakartaLongitude}&method=20&tune=1,1,1,1,1,1,1,1`;
+                var apiUrlErr =
+                    `https://nominatim.openstreetmap.org/reverse?format=json&lat=${jakartaLatitude}&lon=${jakartaLongitude}&accept-language=id`;
+                fetch(solatApiErr)
+                    .then(response => response.json())
+                    .then(data => {
+                        fetch(apiUrlErr)
+                            .then(response => response.json())
+                            .then(location => {
+                                console.log(data.data);
+                                var jadwal = data.data.timings;
+                                var timezone = data.data.meta.timezone;
+                                var city = location.address.city || location.address.city_district;
+                                var country = location.address.country;
+                                displayPrayerTimes(jadwal, timezone, city, country, d);
+                            })
+                            .catch(error => {
+                                // console.error("Error getting city and country: ", error);
+                                const jakartaLatitude = -6.186486;
+                                const jakartaLongitude = 106.834091;
+                                getCityAndCountry(jakartaLatitude, jakartaLongitude);
+                                // Penanganan kesalahan jika gagal mendapatkan informasi kota dan negara
+                            });
+                    })
+                    .catch(error => {
+                        console.error("Error getting city and country: ", error);
+                    });
+
                 // Penanganan kesalahan jika gagal mendapatkan informasi kota dan negara
             });
     }
@@ -119,10 +173,15 @@
         var locationContainer = $('#location');
         var location = city + ', ' + country;
         locationContainer.append('<i class="fa-solid fa-location-dot color-highlight font-14 me-2"></i>' + location);
-        
+
         function displayTime() {
             const currentTime = new Date();
-            const timeOptions = { timeZone: timezone, hour12: false, hour: 'numeric', minute: 'numeric' };
+            const timeOptions = {
+                timeZone: timezone,
+                hour12: false,
+                hour: 'numeric',
+                minute: 'numeric'
+            };
             var timeString = new Intl.DateTimeFormat('id-ID', timeOptions).format(currentTime);
             timeString = timeString.replace(/\./g, ':');
             document.getElementById('time').textContent = timeString;
@@ -130,7 +189,7 @@
         displayTime();
         setInterval(displayTime, 1000);
 
-        
+
         var tgl = date.toLocaleDateString("id-ID", options)
         var tglContainer = $('#tgl-container');
         tglContainer.append(tgl);
@@ -145,7 +204,8 @@
         delete pray.Sunset;
         $.each(pray, function(key, value) {
             html += '<div class="col-6 mb-3"><p class="font-14 color-theme">' + key + '</p></div>';
-            html += '<div class="col-6 mb-3"><p class="font-16 color-theme text-end font-700">' + value + '</p></div>';
+            html += '<div class="col-6 mb-3"><p class="font-16 color-theme text-end font-700">' + value +
+                '</p></div>';
         });
         html += '</div>';
         html += '</div>';
